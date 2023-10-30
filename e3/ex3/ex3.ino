36f6a1617ed57f7643e46ba922275c6dbeb83ff2
mahdie <mahdiiekateb@gmail.com>
2023-10-29 22:26:13 -0700

    مقدار پتاسیومتر را از ورودی بخواند و متناسب با ان شدت روشنایی ال ای دی را تنظظیم کند

--- e3/ex3/ex3.ino
+++ e3/ex3/ex3.ino
@@ -1,13 +1,13 @@
-int led = 9;
+int LED = 9;
 int pt = 3;
 
 void setup() {
-  pinMode(led, OUTPUT);
+  pinMode(LED, OUTPUT);
   pinMode(pt, INPUT);
 }
 
 void loop() {
   int A = analogRead(pt);
   int B = map(A, 0, 1023, 0, 255);
-  analogWrite(led, B);
+  analogWrite(LED, B);
 }
